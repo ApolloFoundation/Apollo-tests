61b8e63cfa4f61ad8f0ca1415ef062923caaa36d
Khabenko <m.habenko@ukr.net>
2020-08-17 13:55:52 +0300

    Update PerformanceSendMoney.scala

--- apl-performance-tests/src/test/scala/com/apollocurrency/PerformanceSendMoney.scala
+++ apl-performance-tests/src/test/scala/com/apollocurrency/PerformanceSendMoney.scala
@@ -61,9 +61,13 @@ class PerformanceSendMoney extends Simulation {
 
 	}
 
+
 	val scn = scenario("Send Money")
+		.exec {session =>
+			session.set("csecret",1)
+		}
 		.exec(http("Get Recipient Account Id")
-			.post(session =>"/apl?requestType=getAccountId&secretPhrase="+(random.nextInt(999)+100000).toString)
+			.post("/apl?requestType=getAccountId&secretPhrase="+(random.nextInt(999)+100000).toString)
 			.check(status.is(200))
 			.check(jsonPath("$.accountRS").find.saveAs("accountRS")))
 		.exec(http("Get Sender Account Id")
@@ -77,6 +81,7 @@ class PerformanceSendMoney extends Simulation {
 			.check(jsonPath("$.tx").find.saveAs("tx"))
 		  .check(bodyString.saveAs("BODY")))
 	.exec (session =>{
+		println(session("csecret").as[Int])
 		val response = session("BODY").as[String]
 		val tx = session("tx").as[String]
 		//println(tx)
@@ -91,7 +96,7 @@ class PerformanceSendMoney extends Simulation {
 			.check(jsonPath("$.errorDescription").notExists.saveAs("errorDescription"))
 		).exec (session =>{
 		val response = session("BODY1").as[String]
-		println(s"Response body: $response")
+		//println(s"Response body: $response")
 		session
 	})
 
